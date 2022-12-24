8e2df112ea64f926ca4d247e21ba30f013614b10
Kate Kulikova <sunndaylive@gmail.com>
2022-12-24 18:09:50 +0300

    Add get all sprites from asset bundle (update AssetBundleProvider), update AssetBundleTester)

--- Assets/Scripts/Runtime/AssetBundles/AssetBundleTester.cs
+++ Assets/Scripts/Runtime/AssetBundles/AssetBundleTester.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using EasyButtons;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,16 +8,19 @@ namespace Runtime.AssetBundles
     public class AssetBundleTester : MonoBehaviour
     {
         [SerializeField] private Text textAssetBundleName;
+        [SerializeField] private Image imageAssetBundleSprite;
         [SerializeField] private string nameAssetBundle;
         
         [Button]
-        public void SetNameAssetBundle()
+        public void SetAssetBundleContent()
         {
             var bundle = AssetBundlesProvider.GetAssetBundleByType(nameAssetBundle);
             
             if (bundle == null) return;
             
             textAssetBundleName.text = bundle.name;
+
+            imageAssetBundleSprite.sprite = AssetBundlesProvider.GetAssetBundleSprites(bundle).FirstOrDefault();
         }
     }
 }
--- Assets/Scripts/Runtime/AssetBundles/AssetBundlesProvider.cs
+++ Assets/Scripts/Runtime/AssetBundles/AssetBundlesProvider.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 namespace Runtime.AssetBundles
@@ -13,6 +15,16 @@ namespace Runtime.AssetBundles
                 Debug.LogError("Asset Bundle with this name does not exist");
             
             return bundle;
+        }
+
+        public static List<Sprite> GetAssetBundleSprites(AssetBundle bundle)
+        {
+            var sprites = bundle.LoadAllAssets<Sprite>();
+            
+            if (sprites == null) 
+                Debug.LogError("This Asset Bundle does not contain sprites");
+
+            return sprites.ToList();
         }
     }
 }
