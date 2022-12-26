bab60d3dc8d0bdfa2b09e7ba82ffc759b2486316
Kate Kulikova <sunndaylive@gmail.com>
2022-12-26 11:42:25 +0300

    Add animations to view logic

--- Assets/Scripts/Runtime/Animations/CustomizationAnimationView.cs
+++ Assets/Scripts/Runtime/Animations/CustomizationAnimationView.cs
@@ -1,4 +1,3 @@
-using System;
 using DG.Tweening;
 using EasyButtons;
 using UnityEngine;
@@ -8,22 +7,18 @@ namespace Runtime.Animations
     public class CustomizationAnimationView : MonoBehaviour
     {
         public float duration;
-        
+
         [SerializeField] private Transform characterTransform;
         [SerializeField] private RectTransform leftPoint;
-        [SerializeField] private RectTransform rightPoint;
 
         private Vector3 _oldPosition;
 
         private void Awake() => _oldPosition = characterTransform.position;
 
         [Button]
-        public void MoveToLeft() => characterTransform.DOMoveX(leftPoint.position.x, duration);
+        public Tween MoveToLeft() => characterTransform.DOMoveX(leftPoint.position.x, duration);
 
         [Button]
-        public void MoveToOldPosition() => characterTransform.DOMoveX(_oldPosition.x, duration);
-
-        [Button]
-        public void MoveToRight() => characterTransform.DOMoveX(rightPoint.position.x, duration);
+        public Tween MoveToOldPosition() => characterTransform.DOMoveX(_oldPosition.x, duration);
     }
 }
--- Assets/Scripts/Runtime/Customization/CustomizationView.cs
+++ Assets/Scripts/Runtime/Customization/CustomizationView.cs
@@ -1,12 +1,24 @@
 using System.Collections.Generic;
+using DG.Tweening;
+using Runtime.Animations;
 using UnityEngine;
 
 namespace Runtime.Customization
 {
     public class CustomizationView : MonoBehaviour
     {
+        [SerializeField] private CustomizationAnimationView animationView;
         [SerializeField] private List<ButtonCustomization> buttons;
         [SerializeField] private List<CustomizationPartView> parts;
+        
+        private Tween _tween;
+        private Sequence _sequence;
+        
+        private Tween _tweenMoveToOldPosition;
+        private Sequence _sequenceMoveToOldPosition;
+        
+        private Tween _tweenMoveToRight;
+        private Sequence _sequenceMoveToRight;
 
         private void Start()
         {
@@ -14,8 +26,27 @@ namespace Runtime.Customization
         }
 
         private void SubcribeEvents() 
-            => buttons.ForEach(b => b.Button.onClick.AddListener(() => SetImageInPart(b)));
+            => buttons.ForEach(b => b.Button.onClick.AddListener(() => StartImageChange(b)));
+        
+        private void StartImageChange(ButtonCustomization button)
+        {
+            if (_sequence != null) _sequence.Kill();
+            
+            _tween?.Kill();
+            _tween = DOVirtual.DelayedCall(0, () => CreateMoveToLeft(button));
+        }
 
+        private void CreateMoveToLeft(ButtonCustomization button)
+        {
+            _sequence = DOTween.Sequence();
+            
+            _sequence.Append(animationView.MoveToLeft());
+            
+            _sequence.Append(animationView.MoveToLeft().OnComplete(() => SetImageInPart(button)));
+            
+            _sequence.Append(animationView.MoveToOldPosition());
+        }
+        
         private void SetImageInPart(ButtonCustomization button)
         {
             var sprite = CustomizationService.GetRandomSpriteByType(button.Type);
